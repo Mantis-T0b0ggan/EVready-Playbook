2c18414cd2b374c6c112d37efaa5fb4476f0b48c
Tim Ducharme <98855446+Mantis-T0b0ggan@users.noreply.github.com>
2025-05-12 11:55:45 -0400

    Update __init__.py

--- rate_importer/utils/__init__.py
+++ rate_importer/utils/__init__.py
@@ -4,18 +4,4 @@ Contains modules for API interactions, database operations,
 and other utility functions.
 """
 
-# Import commonly used functions for easier access
-from .state_utils import get_us_states, get_state_name
-from .api_client import get_utilities, get_schedules_by_utility, get_schedule_detail
-from .db_operations import (
-    insert_utilities, 
-    insert_schedules, 
-    insert_schedule_details, 
-    get_utilities_from_database, 
-    get_schedules_from_database
-)
-
-# This allows imports like:
-# from rate_importer.utils import get_utilities
-# instead of:
-# from rate_importer.utils.api_client import get_utilities
+# Empty init file - we'll use direct imports instead
