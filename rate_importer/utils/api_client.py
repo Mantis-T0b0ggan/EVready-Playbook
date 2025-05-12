5b7af8da15cc891581562b1a97f454f996ee0234
Tim Ducharme <98855446+Mantis-T0b0ggan@users.noreply.github.com>
2025-05-12 11:31:45 -0400

    Create api_client.py

---
+++ rate_importer/utils/api_client.py
@@ -0,0 +1,5 @@
+# RateAcuity API client functions
+# This file will contain functions for interacting with the RateAcuity API
+# - get_utilities_by_state
+# - get_schedule_by_utility
+# - get_schedule_detail
