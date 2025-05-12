0d179146f388c46cb34398a7bd43a40d123a6b7b
Tim Ducharme <98855446+Mantis-T0b0ggan@users.noreply.github.com>
2025-05-12 11:55:34 -0400

    Update __init__.py

--- rate_importer/ui/__init__.py
+++ rate_importer/ui/__init__.py
@@ -3,12 +3,4 @@ UI package for the Rate Importer application.
 Contains modules for different UI components and pages.
 """
 
-# Import commonly used functions for easier access
-from .components import create_header, show_result_message
-from .utilities_page import render_utilities_section
-from .schedules_page import render_schedules_section, render_schedule_details_section
-
-# This allows imports like:
-# from rate_importer.ui import create_header
-# instead of:
-# from rate_importer.ui.components import create_header
+# Empty init file - we'll use direct imports instead
